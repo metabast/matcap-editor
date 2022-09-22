0fbe70bb204913aec59275c6eaad5c072b56c6cb
Sébastien VINCENT <svincent@wanadev.fr>
2022-09-22 11:04:00 +0200

    up config for src absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import glsl from 'vite-plugin-glsl';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve:{
+        alias: {
+            "src": "/src",
+        },
+    },
     plugins: [svelte(), glsl()],
     build: {
         outDir: 'build',
