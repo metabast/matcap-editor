ea1a79b303b4c6a84228ab683e0fc2cbab1db9b0
Sébastien VINCENT <svincent@wanadev.fr>
2022-12-09 17:24:41 +0100

    clear for vuejs

--- src/commands/index.ts
+++
@@ -1,6 +0,0 @@
-export { AddLightCommand } from './AddLightCommand';
-export { SetAmbiantLightCommand } from './SetAmbiantLightCommand';
-export { SetAreaLightPropertyCommand } from './SetAreaLightPropertyCommand';
-export { SetLightModelPropertyCommand } from './SetLightModelPropertyCommand';
-export { SetLightPropertyCommand } from './SetLightPropertyCommand';
-export { SetSphereMaterialParamsCommand } from './SetSphereMaterialParamsCommand';
--- src/commons/Constants.ts
+++
@@ -1,4 +0,0 @@
-export const TAU = Math.PI * 2;
-export const VISOR_OPENED_PERCENT = 0.16;
-
-export const debounceDelay = 400;
