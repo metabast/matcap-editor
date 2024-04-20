6402bae1c69bb0be0c59a202441a8f674c8f2bf2
Sébastien VINCENT <svincent@wanadev.fr>
2024-04-20 13:39:10 +0200

    chore: update package
    add prettier, tailwind and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,28 +4,43 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
     root: true,
     'extends': [
-        'plugin:vue/vue3-essential',
+        'airbnb-base',
+        'plugin:vue/vue3-recommended',
         'eslint:recommended',
         '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
     ],
+    env: {
+        browser: true,
+        node: true,
+    },
+    settings: {
+        'import/resolver': {
+            node: true,
+            typescript: {
+                project: ['tsconfig.app.json'],
+            },
+        },
+    },
     parserOptions: {
         ecmaVersion: 'latest',
     },
     rules: {
-        quotes: [2, 'single', {
-            avoidEscape: true,
-        }],
-        'max-classes-per-file': ['error', 4],
-        'no-param-reassign': 'off',
+        'linebreak-style': ['error', 'unix'],
+        'no-multiple-empty-lines': 'off',
+        'no-use-before-define': 'off',
+        'no-underscore-dangle': 'off',
         'no-plusplus': 'off',
-        'import/prefer-default-export': 'off',
-        'class-methods-use-this': 'off',
-        'indent': ['error', 4, {
-            'SwitchCase': 1,
-        }],
+        'no-multi-spaces': [
+            'error', {
+                'ignoreEOLComments': true,
+            },
+        ],
+        'import/no-extraneous-dependencies': 'off',
         'comma-dangle': [
-            'error',
-            {
+            'error', {
                 'arrays': 'always-multiline',
                 'objects': 'always-multiline',
                 'imports': 'always-multiline',
@@ -33,21 +48,51 @@ module.exports = {
                 'functions': 'ignore',
             },
         ],
-        'semi': ['error', 'always'],
-        'vue/max-attributes-per-line': ['error',
-            {
-                'singleline': {
-                    'max': 1,
-                },
-                'multiline': {
-                    'max': 1,
-                },
+        'object-curly-newline': ['error', {
+            'ObjectPattern': {
+                'multiline': true,
+                'consistent': true,
             },
-        ],
-        'vue/html-closing-bracket-spacing': ['error', {
-            'startTag': 'always',
-            'endTag': 'always',
-            'selfClosingTag': 'always',
+        }],
+        'no-param-reassign': ['error', {
+            'props': false,
+        }],
+        'global-require': 'off',
+        'function-call-argument-newline': 'off',
+        'no-promise-executor-return': 'off',
+        'prefer-regex-literals': 'off',
+        'space-before-blocks': 'error',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-shadow': 'off',
+        'import/no-cycle': 'off',
+        'newline-before-return': 'off',
+        'import/order': ['error', {
+            groups: ['index', 'sibling', 'parent', 'internal', 'external', 'builtin', 'object', 'type'],
+        }],
+        'import/extensions': ['error', 'never', {
+            vue: 'always',
+            json: 'always',
+        }],
+        'lines-between-class-members': ['error', 'always', {
+            exceptAfterSingleLine: true,
+        }],
+        '@typescript-eslint/no-shadow': ['error'],
+        'prefer-destructuring': ['error', {
+            'array': false,
+            'object': true,
+        }],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/no-v-html': 'warn',
+        'vue/no-unused-refs': 'error',
+        'vue/component-api-style': ['warn', ['script-setup', 'options']],
+        'vue/block-lang': ['error', {
+            script: {
+                lang: 'ts',
+            },
+        }],
+        'vue/component-tags-order': ['error', {
+            order: ['script', 'template', 'style'],
         }],
     },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,vue}', 'index.html'],
+    theme: {
+        extend: {
+            colors: {
+            },
+        },
+    },
+    plugins: [],
+};
+
